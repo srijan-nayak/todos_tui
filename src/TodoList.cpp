8882d1edd0fc64aab48eba060bcbf7aa55594022
Srijan Nayak <srijannayak0@gmail.com>
2022-10-23 18:18:34 +0530

    feat: Implement TodoList::AddNewTodoItem method

--- src/TodoList.cpp
+++ src/TodoList.cpp
@@ -4,11 +4,20 @@
 
 #include "TodoList.h"
 
+#include "TodoItem.h"
+
+#include <string>
+#include <vector>
+#include <utility>
+
 namespace todos {
 
 std::vector<TodoItem> TodoList::GetTodoItems() const { return _todo_items; }
 
-void TodoList::AddNewTodoItem(std::string todo_text) {}
+void TodoList::AddNewTodoItem(std::string todo_text) {
+  TodoItem new_todo_item{++_todo_item_id_counter, std::move(todo_text), false};
+  _todo_items.emplace_back(new_todo_item);
+}
 
 void TodoList::RemoveTodoItem(int id) {}
 
