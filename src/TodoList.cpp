9caff5f96c12386cbe1519ccbd17056e9d22a012
Srijan Nayak <srijannayak0@gmail.com>
2022-10-23 18:11:11 +0530

    feat: Create TodoList class

---
+++ src/TodoList.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Srijan Nayak on 23-10-2022.
+//
+
+#include "TodoList.h"
+
+namespace todos {
+
+std::vector<TodoItem> TodoList::GetTodoItems() { return {}; }
+
+void TodoList::AddNewTodoItem(std::string todo_text) {}
+
+void TodoList::RemoveTodoItem(int id) {}
+
+void TodoList::ToggleTodoItemIsComplete(int id) {}
+
+} // namespace todos
---
+++ src/TodoList.h
@@ -0,0 +1,32 @@
+//
+// Created by Srijan Nayak on 23-10-2022.
+//
+
+#ifndef TODOS_TUI_SRC_TODOLIST_H_
+#define TODOS_TUI_SRC_TODOLIST_H_
+
+#include "TodoItem.h"
+
+#include <string>
+#include <vector>
+
+namespace todos {
+
+class TodoList {
+public:
+  std::vector<TodoItem> GetTodoItems();
+
+  void AddNewTodoItem(std::string todo_text);
+
+  void RemoveTodoItem(int id);
+
+  void ToggleTodoItemIsComplete(int id);
+
+private:
+  std::vector<TodoItem> _todo_items;
+  static int _todo_item_id_counter;
+};
+
+} // namespace todos
+
+#endif // TODOS_TUI_SRC_TODOLIST_H_
