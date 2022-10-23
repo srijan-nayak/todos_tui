517d9e401c46f81b7cefac55ec77a3e146e27aa7
Srijan Nayak <srijannayak0@gmail.com>
2022-10-23 12:45:44 +0530

    refactor: Remove _id field from TodoItem class

--- src/TodoItem.cpp
+++ src/TodoItem.cpp
@@ -8,10 +8,9 @@
 
 namespace todos {
 
-TodoItem::TodoItem(int id, std::string todo_text, bool is_completed)
-    : _id(id), _todo_text(std::move(todo_text)), _is_completed(is_completed) {}
+TodoItem::TodoItem(std::string todo_text, bool is_completed)
+    : _todo_text(std::move(todo_text)), _is_completed(is_completed) {}
 
-int TodoItem::GetId() const { return _id; }
 const std::string &TodoItem::GetTodoText() const { return _todo_text; }
 bool TodoItem::IsIsCompleted() const { return _is_completed; }
 
--- src/TodoItem.h
+++ src/TodoItem.h
@@ -11,14 +11,12 @@ namespace todos {
 
 class TodoItem {
 private:
-  int _id;
   std::string _todo_text;
   bool _is_completed;
 
 public:
-  TodoItem(int id, std::string todo_text, bool is_completed);
+  TodoItem(std::string todo_text, bool is_completed);
 
-  [[nodiscard]] int GetId() const;
   [[nodiscard]] const std::string &GetTodoText() const;
   [[nodiscard]] bool IsIsCompleted() const;
 
