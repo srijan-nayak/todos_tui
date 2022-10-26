85ed14eb325c39bcf045c98b6cd7ff93484c3a79
Srijan Nayak <srijannayak0@gmail.com>
2022-10-26 12:36:18 +0530

    feat: Implement main function

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,10 @@
-int main() {}
+#include "todos/TodoList.h"
+#include "todos/TodoListFileManager.h"
+#include "tui/TodosTui.h"
+
+int main() {
+  todos::TodoList todo_list{todos::TodoListFileManager::ReadTodoItems()};
+  tui::TodosTui todos_tui{todo_list};
+  todos_tui.StartLoop();
+  todos::TodoListFileManager::WriteTodoItems(todo_list.GetTodoItems());
+}
